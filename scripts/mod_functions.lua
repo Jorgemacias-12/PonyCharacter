002192f279458a221b04f04f8831f8ab7e6e719c
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2023-12-13 03:55:24 -0600

    Add: mod_functions.lua
    	This file contains all the functions that are required by the
    	events used by the mod.
    
    	#1

---
+++ scripts/mod_functions.lua
@@ -0,0 +1,32 @@
+local mod_items = require("mod_items")
+local pony = require("pony")
+
+local mod_functions = {}
+
+function mod_functions.playerInit(player)
+  if player:GetName() ~= "Pony" then
+    return
+  end
+
+  player:AddCollectible(mod_items.DragonHoardItem, 0, true);
+  player:AddNullCostume(pony.dragon_hoard_costume_id)
+  player:AddNullCostume(pony.body_costume_id)
+
+end
+
+function mod_functions.onUpdate()
+end
+
+function mod_functions.postPeffectUpdate()
+end
+
+function mod_functions.entityTakeDmg(entity, dmg_amount, dmg_flag, dmg_src, dmg_countdown)
+end
+
+function mod_functions.useItem(collectibleType, charge, player)
+end
+
+function mod_functions.evaluateCache(player, cacheFlag)
+end
+
+return mod_functions
