b7608554ed2836ecf824d0f8dbb046be98ea1948
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2023-12-13 04:41:05 -0600

    Update: pony.lua
    	#1 Implement applyCostume function into pony module

--- scripts/pony.lua
+++ scripts/pony.lua
@@ -1,3 +1,5 @@
+local utils = require("scripts.utils")
+
 local pony = {}
 
 pony.hair_costume_id = Isaac.GetCostumeIdByPath("gfx/characters/PonyHair.anm2")
@@ -10,5 +12,36 @@ pony.custom_infestation_costume_id = Isaac.GetCostumeIdByPath("gfx/characters/cu
 pony.custom_libra_costume_id = Isaac.GetCostumeIdByPath("gfx/characters/custom_libra.anm2")
 pony.custom_uranus_costume_id = Isaac.GetCostumeIdByPath("gfx/characters/custom_uranus.anm2")
 pony.custom_varicose_veins_costume_id = Isaac.GetCostumeIdByPath("gfx/characters/custom_varicoseveins.anm2")
+
+function pony.applyCostume(player)
+  if player:GetName() ~= "Pony" then
+    return
+  end
+
+  local flyingItems = {
+    CollectibleType.COLLECTIBLE_FATE,
+    CollectibleType.COLLECTIBLE_HOLY_GRAIL,
+    CollectibleType.COLLECTIBLE_DOGMA,
+    CollectibleType.COLLECTIBLE_LORD_OF_THE_PIT,
+    CollectibleType.COLLECTIBLE_REVELATION
+  }
+
+  if player.CanFly then
+    player:AddNullCostume(pony.hair_costume_id)
+
+    if player:HasCollectible(CollectibleType.COLLECTIBLE_BRIMSTONE) then
+      player:AddNullCostume(pony.dragon_wings_brimstone_costume_id)
+    elseif utils.hasAnyCollectible(player, flyingItems) then
+      player:AddNullCostume(pony.dragon_wings_costume_id)
+    end
+  
+  else
+    player:TryRemoveNullCostume(pony.dragon_wings_costume_id)
+    player:TryRemoveNullCostume(pony.dragon_wings_brimstone_costume_id)
+    player:AddNullCostume(pony.body_costume_id)
+    player:AddNullCostume(pony.hair_costume_id)
+  end
+
+end
 
 return pony
