e05052a0352aae3fb399bfab892ab314677559de
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2023-12-13 03:51:43 -0600

    Update: main.lua
    	#1 Improves mod entrypoint code by using modules to separate
    	   logic and textures and sounds application to the character.

--- main.lua
+++ main.lua
@@ -1,50 +1,30 @@
-local Pony = require('./scripts/Pony')
+-- Import the module containing function definitions for the mod's callbacks.
+-- This assumes that 'mod_functions.lua' contains the necessary logic for each event.
+local mod_functions = require("scripts.mod_functions")
+local mod_items = require('scripts.mod_items')
+-- Register the mod with the name "Pony the Dragon! [REP]" and a version number of 1.
+local Mod = RegisterMod("Pony the Dragon! [REP]", 1)
 
-local PonyCharacterMod = RegisterMod("PonyCharacter", 1)
+-- Get a reference to the game object.
+local game = Game()
 
-function PonyCharacterMod:player_init(entity_player)
-	local player = Isaac.GetPlayer()
+-- Register various callbacks with the game engine. Each callback is linked to a specific function
+-- in the 'mod_functions.lua' file, handling different aspects of the game's events.
 
-	if player:GetName() == Pony.Name then
-		Pony.InitPony(Pony)
-	end
-end
+-- This callback is triggered after a player character is initialized in the game.
+Mod:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, mod_functions.playerInit)
 
-function PonyCharacterMod:onUpdate()
-	Pony.OnUpdate(Pony)
-end
+-- This callback is triggered every game update (typically, this occurs every frame).
+Mod:AddCallback(ModCallbacks.MC_POST_UPDATE, mod_functions.onUpdate)
 
-function PonyCharacterMod:update(currentPlayer)
-	Pony.Update(Pony)
-end
+-- This callback is triggered after the effects of a passive item are updated.
+Mod:AddCallback(ModCallbacks.MC_POST_PEFFECT_UPDATE, mod_functions.postPeffectUpdate)
 
-function PonyCharacterMod:TakeDamage(entity, dmg_amount, dmg_flag, dmg_src, dmg_countdown)
-	Pony.TakeDamage(Pony)
-end
+-- This callback is triggered when an entity takes damage. It specifically listens for damage taken by the player.
+Mod:AddCallback(ModCallbacks.MC_ENTITY_TAKE_DMG, mod_functions.entityTakeDmg, EntityType.ENTITY_PLAYER)
 
-function PonyCharacterMod:ActiveDragonWings()
-	Pony.ActivateWings(Pony)
-end
+-- This callback is triggered when a specific item is used. Replace 'your_item_id' with the actual item ID.
+Mod:AddCallback(ModCallbacks.MC_USE_ITEM, mod_items.DragonWingsItem)
 
-function PonyCharacterMod:onCache()
-	Pony.OnCache(Pony)
-end
-
--- Init Callback verification to mod init
-PonyCharacterMod:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, PonyCharacterMod.player_init)
-
--- Init callback to update mod
-PonyCharacterMod:AddCallback(ModCallbacks.MC_POST_UPDATE, PonyCharacterMod.onUpdate);
-
--- Update game effects when DragonHoard is picked up
-PonyCharacterMod:AddCallback(ModCallbacks.MC_POST_PEFFECT_UPDATE, PonyCharacterMod.update)
-
--- Event when player gets hit
-PonyCharacterMod:AddCallback(ModCallbacks.MC_ENTITY_TAKE_DMG, PonyCharacterMod.TakeDamage, EntityType.ENTITY_PLAYER)
-
--- Event when DragonWings are going to be used
-PonyCharacterMod:AddCallback(ModCallbacks.MC_USE_ITEM, PonyCharacterMod.ActivateDragonWings,
-	PonyCharacterMod.ActivateDragonWings)
-
--- Event raised every frame to update player statistics
-PonyCharacterMod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, Pony.onCache)
+-- This callback is used to re-evaluate certain cached values, like player stats, whenever they need updating.
+Mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, mod_functions.evaluateCache)
