67d140d0772257c354154de09e353a9ec1e11632
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2023-12-14 05:54:52 -0600

    Update: mod_items.lua
    	Added `itemsToCostumeMap` map that has a reference to
    	Collectible and is used by pony module to apply the desired
    	costume depending of the collectible in use.

--- scripts/mod_items.lua
+++ scripts/mod_items.lua
@@ -3,4 +3,53 @@ local mod_items = {}
 mod_items.DragonHoardItem = Isaac.GetItemIdByName("Dragon Hoard");
 mod_items.DragonWingsItem = Isaac.GetItemIdByName("Dragon Wings");
 
+mod_items.itemsToCostumeMap = {
+  [CollectibleType.COLLECTIBLE_BRIMSTONE] = {
+    applyWhenFly = true,
+    Isaac.GetCostumeIdByPath("gfx/characters/PonyHair.anm2"),
+    Isaac.GetCostumeIdByPath("gfx/characters/DragonWingsBrimstone.anm2"),
+  },
+  [CollectibleType.COLLECTIBLE_FATE] = {
+    applyWhenFly = true,
+    Isaac.GetCostumeIdByPath("gfx/characters/PonyBody.anm2"),
+    Isaac.GetCostumeIdByPath("gfx/characters/PonyHair.anm2")
+  },
+  [CollectibleType.COLLECTIBLE_HOLY_GRAIL] = {
+    applyWhenFly = true,
+    Isaac.GetCostumeIdByPath("gfx/characters/PonyBody.anm2"),
+    Isaac.GetCostumeIdByPath("gfx/characters/PonyHair.anm2"),
+  },
+  [CollectibleType.COLLECTIBLE_DOGMA] = {
+    applyWhenFly = true,
+    Isaac.GetCostumeIdByPath("gfx/characters/PonyBody.anm2"),
+    Isaac.GetCostumeIdByPath("gfx/characters/PonyHair.anm2"),
+  },
+  [CollectibleType.COLLECTIBLE_LORD_OF_THE_PIT] = {
+    applyWhenFly = true,
+    Isaac.GetCostumeIdByPath("gfx/characters/PonyBody.anm2"),
+    Isaac.GetCostumeIdByPath("gfx/characters/PonyHair.anm2"),
+  },
+  [CollectibleType.COLLECTIBLE_REVELATION] = {
+    applyWhenFly = true,
+    Isaac.GetCostumeIdByPath("gfx/characters/PonyBody.anm2"),
+    Isaac.GetCostumeIdByPath("gfx/characters/PonyHair.anm2"),
+  },
+  [CollectibleType.COLLECTIBLE_INFESTATION] = {
+    applyWhenFly = false,
+    Isaac.GetCostumeIdByPath("gfx/characters/custom_infestation.anm2")
+  },
+  [CollectibleType.COLLECTIBLE_LIBRA] = {
+    applyWhenFly = false,
+    Isaac.GetCostumeIdByPath("gfx/characters/custom_libra.anm2")
+  },
+  [CollectibleType.COLLECTIBLE_VARICOSE_VEINS] = {
+    applyWhenFly = false,
+    Isaac.GetCostumeIdByPath("gfx/characters/custom_varicoseveins.anm2")
+  },
+  [CollectibleType.COLLECTIBLE_URANUS] = {
+    applyWhenFly = false,
+    Isaac.GetCostumeIdByPath("gfx/characters/custom_uranus.anm2")
+  },
+}
+
 return mod_items;
