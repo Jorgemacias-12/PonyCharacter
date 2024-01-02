b7c4830575ddc2371a48f826cfd0f4ebb7583bd5
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2024-01-02 17:48:52 -0600

    Update: costumes module
    	Add more tables that contains skins data.

--- scripts/costumes.lua
+++ scripts/costumes.lua
@@ -1,126 +1,192 @@
-local itemCostumes = {
+local costumes = {}
+
+costumes.itemCostumes = {
   {
-    collectibleType = "COLLECTIBLE_PONY",
+    collectibleType = CollectibleType.COLLECTIBLE_PONY,
     spritePaths = {
       withoutWings = "gfx/characters/costumes/costume_pony.png",
       withWings = "gfx/characters/costumes/costume_ponywings.png"
     }
   },
   {
-    collectibleType = "COLLECTIBLE_WHITE_PONY",
+    collectibleType = CollectibleType.COLLECTIBLE_WHITE_PONY,
     spritePaths = {
       withoutWings = "gfx/characters/costumes/costume_whitepony.png",
       withWings = "gfx/characters/costumes/costume_whiteponywings.png"
     }
   },
   {
-    collectibleType = "COLLECTIBLE_SPEED_BALL",
+    collectibleType = CollectibleType.COLLECTIBLE_SPEED_BALL,
     spritePaths = {
       default = "gfx/characters/costumes/costume_speedball.png"
     }
   },
   {
-    collectibleType = "COLLECTIBLE_GODS_FLESH",
+    collectibleType = CollectibleType.COLLECTIBLE_GODS_FLESH,
     spritePaths = {
       default = "gfx/characters/costumes/costume_godsflesh.png"
     }
   },
   {
-    collectibleType = "COLLECTIBLE_HAEMOLACRIA",
+    collectibleType = CollectibleType.COLLECTIBLE_HAEMOLACRIA,
     spritePaths = {
       default = "gfx/characters/costumes/costume_haemolacria.png"
     }
   },
   {
-    collectibleType = "COLLECTIBLE_MUTANT_SPIDER",
+    collectibleType = CollectibleType.COLLECTIBLE_MUTANT_SPIDER,
     spritePaths = {
       default = "gfx/characters/costumes/costume_mutantspider.png"
     }
   },
   {
-    collectibleType = "COLLECTIBLE_PIGGY_BANK",
+    collectibleType = CollectibleType.COLLECTIBLE_PIGGY_BANK,
     spritePaths = {
       default = "gfx/characters/costumes/costume_piggybank.png"
     }
   },
   {
-    collectibleType = "COLLECTIBLE_PLACENTA",
+    collectibleType = CollectibleType.COLLECTIBLE_PLACENTA,
     spritePaths = {
       default = "gfx/characters/costumes/costume_placenta.png"
     }
   },
   {
-    collectibleType = "COLLECTIBLE_WOODEN_SPOON",
+    collectibleType = CollectibleType.COLLECTIBLE_WOODEN_SPOON,
     spritePaths = {
       default = "gfx/characters/costumes/costume_woodenspoon.png"
     }
   },
   {
-    collectibleType = "COLLECTIBLE_CHAOS",
+    collectibleType = CollectibleType.COLLECTIBLE_CHAOS,
     spritePaths = {
       default = "gfx/characters/costumes/costume_chaos.png"
     }
   },
   {
-    collectibleType = "COLLECTIBLE_BLOOD_CLOT",
+    collectibleType = CollectibleType.COLLECTIBLE_BLOOD_CLOT,
     spritePaths = {
       default = "gfx/characters/costumes/costume_bloodclot.png"
     }
   },
   {
-    collectibleType = "COLLECTIBLE_GIMPY",
+    collectibleType = CollectibleType.COLLECTIBLE_GIMPY,
     spritePaths = {
       default = "gfx/characters/costumes/costume_gimpy.png"
     }
   },
   {
-    collectibleType = "COLLECTIBLE_MOMS_WIG",
+    collectibleType = CollectibleType.COLLECTIBLE_MOMS_WIG,
     spritePaths = {
       default = "gfx/characters/costumes/costume_momswig.png"
     }
   },
   {
-    collectibleType = "COLLECTIBLE_SQUEEZY",
+    collectibleType = CollectibleType.COLLECTIBLE_SQUEEZY,
     spritePaths = {
       default = "gfx/characters/costumes/costume_squeezy.png"
     }
   },
   {
-    collectibleType = "COLLECTIBLE_ANALOG_STICK",
+    collectibleType = CollectibleType.COLLECTIBLE_ANALOG_STICK,
     spritePaths = {
       default = "gfx/characters/costumes/costume_analogstick.png"
     }
   },
   {
-    collectibleType = "COLLECTIBLE_IMMACULATE_HEART",
+    collectibleType = CollectibleType.COLLECTIBLE_IMMACULATE_HEART,
     spritePaths = {
       default = "gfx/characters/costumes/costume_immaculateheart.png"
     }
   },
   {
-    collectibleType = "COLLECTIBLE_VIRUS",
+    collectibleType = CollectibleType.COLLECTIBLE_VIRUS,
     spritePaths = {
       default = "gfx/characters/costumes/costume_virus.png"
     }
   },
   {
-    collectibleType = "COLLECTIBLE_PAGEANT_BOY",
+    collectibleType = CollectibleType.COLLECTIBLE_PAGEANT_BOY,
     spritePaths = {
       default = "gfx/characters/costumes/costume_pageantboy.png"
     }
   },
   {
-    collectibleType = "COLLECTIBLE_JESUS_JUICE",
+    collectibleType = CollectibleType.COLLECTIBLE_JESUS_JUICE,
     spritePaths = {
       default = "gfx/characters/costumes/costume_jesusjuice.png"
     }
   },
   {
-    collectibleType = "COLLECTIBLE_ROCK_BOTTOM",
+    collectibleType = CollectibleType.COLLECTIBLE_ROCK_BOTTOM,
     spritePaths = {
       default = "gfx/characters/costumes/costume_rockbottom.png"
     }
   }
 }
 
-return itemCostumes;
+costumes.nonFlyingCostumes = {
+  [CollectibleType.COLLECTIBLE_INFESTATION] = {
+    removeExisting = true,
+    customCostume = Isaac.GetCostumeIdByPath("gfx/characters/custom_infestation.anm2")
+  },
+  [CollectibleType.COLLECTIBLE_LIBRA] = {
+    removeExisting = true,
+    customCostume = Isaac.GetCostumeIdByPath("gfx/characters/custom_libra.anm2")
+  },
+  [CollectibleType.COLLECTIBLE_VARICOSE_VEINS] = {
+    removeExisting = true,
+    customCostume = Isaac.GetCostumeIdByPath("gfx/characters/custom_varicoseveins.anm2")
+  },
+  [CollectibleType.COLLECTIBLE_CEREMONIAL_ROBES] = {
+    removeExisting = true,
+    customCostume = Isaac.GetCostumeIdByPath("gfx/characters/PonyHair.anm2")
+  },
+  [CollectibleType.COLLECTIBLE_BLOOD_OF_THE_MARTYR] = {
+    removeExisting = true,
+    customCostume = Isaac.GetCostumeIdByPath("gfx/characters/PonyHair.anm2")
+  },
+  [CollectibleType.COLLECTIBLE_HALO] = {
+    removeExisting = true,
+    customCostume = Isaac.GetCostumeIdByPath("gfx/characters/PonyHair.anm2")
+  },
+  [CollectibleType.COLLECTIBLE_MERCURIUS] = {
+    removeExisting = true,
+    customCostume = Isaac.GetCostumeIdByPath("gfx/characters/PonyHair.anm2")
+  },
+  [CollectibleType.COLLECTIBLE_URANUS] = {
+    removeExisting = true,
+    customCostume = Isaac.GetCostumeIdByPath("gfx/characters/custom_uranus.anm2")
+  }
+}
+
+costumes.flyingCostumes = {
+  {
+    collectibleType = CollectibleType.COLLECTIBLE_BRIMSTONE,
+    costume = {
+      customLayer = Isaac.GetCostumeIdByPath("gfx/characters/DragonWingsBrimstone.anm2")
+    },
+  },
+  {
+    collectibleType = CollectibleType.COLLECTIBLE_FATE,
+    costume = nil
+  },
+  {
+    collectibleType = CollectibleType.COLLECTIBLE_BRIMSTONE,
+    costume = nil,
+  },
+  {
+    collectibleType = CollectibleType.COLLECTIBLE_BRIMSTONE,
+    costume = nil
+  },
+  {
+    collectibleType = CollectibleType.COLLECTIBLE_BRIMSTONE,
+    costume = nil,
+  },
+  {
+    collectibleType = CollectibleType.COLLECTIBLE_BRIMSTONE,
+    costume = nil,
+  },
+}
+
+return costumes;
